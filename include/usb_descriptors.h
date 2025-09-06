a188b944c43dd2078c70549b7be21e4b4229cb54
David Álvarez Rosa <david@alvarezrosa.com>
2025-09-06 22:43:11 +0100

    this is moving the mouse

--- include/usb_descriptors.h
+++ include/usb_descriptors.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2025 David Alvarez Rosa (david.alvarezrosa.com)
+ * Copyright (c) 2019 Ha Thach (tinyusb.org)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,17 +20,18 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *
  */
 
-#pragma once
+#ifndef USB_DESCRIPTORS_H_
+#define USB_DESCRIPTORS_H_
 
-#include <cstdint>
-
-enum class ReportId : uint8_t {
+enum
+{
   REPORT_ID_KEYBOARD = 1,
   REPORT_ID_MOUSE,
   REPORT_ID_CONSUMER_CONTROL,
   REPORT_ID_GAMEPAD,
   REPORT_ID_COUNT
 };
+
+#endif /* USB_DESCRIPTORS_H_ */
